4f1ececca7eef587a75270d3cc7523fa5a83d03e
Mark <you@example.com>
2023-04-03 14:32:56 -0700

    editing

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,11 +7,10 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
 
-	for (i = 0; a[i][7]; i++)
+	for (int i = 0; a[i][7]; i++)
 	{
-		for (j = 0; j < 8; j++)
+		for (int j = 0; j < 8; j++)
 			_putchar(a[i][j]);
 		_putchar('\n');
 	}
